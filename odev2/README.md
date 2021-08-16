6bc78fabaefe75e313c2c45c157634b04eb008d8
Cagla <caglaeyubog@gmail.com>
2021-08-16 14:24:05 +0300

    Local storage geliştirilmeye başlandı

--- odev2/README.md
+++ odev2/README.md
@@ -4,6 +4,8 @@
 
 Kodluyoruz'un Front-end Eğitiminin JavaScript bölümünde istenen To Do List geliştirme ödevidir. CSS ve HTML dosyaları hazır olarak verilmiş, JavaScript kodları benim tarafımdan yazılmıştır.
 
+Önemli Not: Local Storage eklenmiş olup, hala geliştirilme aşamasındadır.
+
 ![todolist](https://raw.githubusercontent.com/Kodluyoruz/taskforce/main/javascript/javascript-temel/odev2/figures/todolist.gif)
 
 
--- odev2/todolist/js/todo.js
+++ odev2/todolist/js/todo.js
@@ -7,28 +7,33 @@ listItem.addEventListener( "click", function (e) {
 );
 
 //function to add new item to the end of the list
+var i=0;
 function addItem() {
     var li = document.createElement("li");
     var input = document.getElementById("task").value;
     var t = document.createTextNode(input);
+    i = localStorage.length;
 
     li.appendChild(t);
 
     if (input === "")
-        {$(".error").toast("show");}
+        {$(".error").toast("show");
+        }
     else {   
         $(".success").toast("show");
         document.getElementById("list").appendChild(li);
+        localStorage.setItem(i+"item", input);
+        i++;
         }
     document.getElementById("task").value = "";
-
+          
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
     span.className = "close";
     span.appendChild(txt);
     li.appendChild(span);
     removeItem();
-
+    
 };
 
 //close button function to remove items
@@ -39,8 +44,26 @@ function removeItem() {
         delItem[i].onclick = function() {
             var div = this.parentElement;
             div.style.display = "none";
-        }
-    };
+            clearLocal();
+    }};  
 }
 
+
+function getItem() {
+    let local="";
+    let count;
+    let html = "";
+    for (count = 0; count<localStorage.length; count++){
+        local = localStorage.getItem(count+"item");
+        html += `<li><span>${local}</span><span class="close">×</span></li>`;
+    }
+    listItem.innerHTML += html;
+}
+
+function clearLocal() {
+      localStorage.clear();
+}
+
+
+getItem();
 removeItem();
